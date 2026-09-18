fe938e6da60a14e7d38e4eaa944aeec8c1750f41
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-18 08:04:46 +0200

    feat: expose provisioning public API

---
+++ src/pyiamkit/provisioning/__init__.py
@@ -0,0 +1,62 @@
+"""Provisioning and SCIM public API."""
+
+from .application import ProvisioningSource, ScimProvisioningService
+from .domain import (
+    ProvisioningResourceId,
+    ProvisioningResourceStatus,
+    ProvisioningUser,
+)
+from .errors import (
+    InvalidProvisioningResource,
+    InvalidScimRequest,
+    ProvisioningConflict,
+    ProvisioningError,
+    ProvisioningManagedStateConflict,
+    ProvisioningPreconditionFailed,
+    ProvisioningResourceNotFound,
+    UnsupportedScimPatch,
+)
+from .ports import ProvisioningUserRepository
+from .scim import (
+    SCIM_LIST_RESPONSE_SCHEMA,
+    SCIM_PATCH_SCHEMA,
+    SCIM_USER_SCHEMA,
+    ScimEmail,
+    ScimListResponse,
+    ScimMeta,
+    ScimName,
+    ScimPatchOperation,
+    ScimPatchVerb,
+    ScimUserInput,
+    ScimUserResource,
+    apply_user_patch,
+)
+
+__all__ = [
+    "InvalidProvisioningResource",
+    "InvalidScimRequest",
+    "ProvisioningConflict",
+    "ProvisioningError",
+    "ProvisioningManagedStateConflict",
+    "ProvisioningPreconditionFailed",
+    "ProvisioningResourceId",
+    "ProvisioningResourceNotFound",
+    "ProvisioningResourceStatus",
+    "ProvisioningSource",
+    "ProvisioningUser",
+    "ProvisioningUserRepository",
+    "SCIM_LIST_RESPONSE_SCHEMA",
+    "SCIM_PATCH_SCHEMA",
+    "SCIM_USER_SCHEMA",
+    "ScimEmail",
+    "ScimListResponse",
+    "ScimMeta",
+    "ScimName",
+    "ScimPatchOperation",
+    "ScimPatchVerb",
+    "ScimProvisioningService",
+    "ScimUserInput",
+    "ScimUserResource",
+    "UnsupportedScimPatch",
+    "apply_user_patch",
+]
